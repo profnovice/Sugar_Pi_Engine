8637eb266f02017070095eb47326ca9ce4796e78
Matthew John Stepita <mattomg24@gmail.com>
2025-11-11 12:54:25 -0800

    create simple test class for Entity create simple test shape in main drawn to window have test shape move

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,42 @@
 #include <SFML/Graphics.hpp>
 //Hello GitWorld!
+
+class Entity 
+{
+    int m_entityID;
+
+
+public:
+    Entity(int entityID)
+        :m_entityID(entityID)
+    {
+
+
+    }
+    const int getEntityID()
+    {
+        return m_entityID;
+    }
+
+
+};
+
+
+
+
+
+
 int main()
 {
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
+
+    int circleRadius = 64;
+    sf::CircleShape myShape(circleRadius);
+    myShape.setPosition({512,512});
+    sf::Color newColor(255, 255, 255);
+    myShape.setFillColor(newColor);
+
 
     while (window.isOpen())
     {
@@ -13,9 +46,22 @@ int main()
             {
                 window.close();
             }
-        }
 
+           
+     
+        }
+        
         window.clear();
+        //magical draw area
+
+        window.draw(myShape);
+
+        myShape.setPosition({ myShape.getPosition().x, myShape.getPosition().y - 1});
+
+
+
+
+        //end magical draw area
         window.display();
     }
 }
