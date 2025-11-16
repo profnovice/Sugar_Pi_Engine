de045e3c538204a26ebf89af4b50c33b19b3dfa6
Matthew John Stepita <mattomg24@gmail.com>
2025-11-16 12:15:11 -0800

    create operators for Vec2 class

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -12,3 +12,39 @@ Vec2::Vec2(float x_in, float y_in)
 	
 }
 
+bool Vec2::operator==(const Vec2& rhs) const
+{
+	if (x == rhs.x && y == rhs.y)
+	{
+		return true;
+	}
+	return false;
+}
+
+Vec2 Vec2::operator+(const Vec2& rhs) const
+{
+	return Vec2(x+rhs.x,y + rhs.y);
+}
+
+Vec2 Vec2::operator-(const Vec2& rhs) const
+{
+	return Vec2(x - rhs.x, y - rhs.y);
+}
+
+Vec2 Vec2::operator*(const float& val) const
+{
+	return Vec2(x * val, y * val);
+}
+
+void Vec2::operator+=(const Vec2& rhs)
+{
+	x = x + rhs.x;
+	y = y + rhs.y;
+}
+
+void Vec2::operator-=(const Vec2& rhs)
+{
+	x = x - rhs.x;
+	y = y - rhs.y;
+}
+
--- src/Vec2.h
+++ src/Vec2.h
@@ -6,6 +6,12 @@ public:
 
 	Vec2();
 	Vec2(float, float);
+	bool operator == (const Vec2& rhs) const;
+	Vec2 operator + (const Vec2& rhs) const;
+	Vec2 operator - (const Vec2& rhs) const;
+	Vec2 operator * (const float& val) const;
+	void operator +=(const Vec2 & rhs);
+	void operator -=(const Vec2& rhs);
 
 
 };
