16d8d1576544d14c79b277b711d2d48b026498bd
Matthew John Stepita <mattomg24@gmail.com>
2025-11-27 17:22:56 -0800

    Add git commit best practices comment to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,16 @@
 #include "EntityManager.h"
 
 
+/*
+Separate subject from body with a blank line
+Limit the subject line to 50 characters
+Capitalize the subject line
+Do not end the subject line with a period
+Use the imperative mood in the subject line //if applied this commit will
+Wrap the body at 72 characters
+Use the body to explain what and why vs. how
+*/
+
 //Lecture 3
 /*
 Lecture 3:
