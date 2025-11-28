881326528e44744947b670652570e793df598404
Matthew John Stepita <mattomg24@gmail.com>
2025-11-28 12:41:38 -0800

    feat(src): Add more function declarations and definition outlines

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,2 +1,53 @@
 #include "Game.h"
 
+void Game::run()
+{
+	while (m_running)
+	{
+		m_entities.update();
+
+		if (!m_paused)
+		{
+			sEnemySpawner();
+			sMovement();
+			sCollision();
+		}
+		sUserInput();
+		sRender();
+
+		m_currentFrame++;//could be affected by pause
+
+	}
+}
+
+void Game::sMovement()
+{
+}
+
+void Game::sUserInput()
+{
+}
+
+void Game::sRender()
+{
+}
+
+void Game::sEnemySpawner()
+{
+}
+
+void Game::sCollision()
+{
+}
+
+void Game::spawnPlayer()
+{
+}
+
+void Game::spawnEnemy(SimpEntPtr entity)
+{
+}
+
+void Game::spawnProjectile(SimpEntPtr entity)
+{
+}
--- src/Game.h
+++ src/Game.h
@@ -10,24 +10,37 @@ class Game
 {
 	sf::RenderWindow m_window;
 	EntityManager m_entities;
+	sf::Font m_font;
+	sf::Text m_text;
 	SimpEntPtr Player;
+	int m_score = 0;
+	int m_currentFrame = 0;
 	bool m_paused;
 	bool m_running;
 	bool m_showColliders;
 	Vec2 m_windowOffset;
 
 	template <typename T>
-	void drawWrapper(T);
-
-public:
-	Game(std::string);
+	void drawWrapper(T); //Not sure I'll need this just preemptive
 	~Game();
-	void run();
-	void init();
-	void update();
+	void setPaused(bool paused);
+	
+	void init();//read in config, create window with settings, call spawn player
+	//std::ifstream fin(path);
+
 	void sMovement();
 	void sUserInput();
 	void sRender();
 	void sEnemySpawner();
 	void sCollision();
+
+	void spawnPlayer();
+	void spawnEnemy(SimpEntPtr entity);
+	void spawnProjectile(SimpEntPtr entity);
+
+	public:
+	Game(const std::string & config);
+	void run();
+
 };
+
