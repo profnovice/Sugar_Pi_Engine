b269af210e32a709595e0365ddd50ed315858774
Matthew John Stepita <mattomg24@gmail.com>
2025-11-28 17:54:01 -0800

    refac(src): Make the SimpleEntity more private
    This way only the entity manager can make changes.

--- src/SimpleEntity.cpp
+++ src/SimpleEntity.cpp
@@ -1,7 +1,26 @@
 #include "SimpleEntity.h"
 
-
 SimpleEntity::SimpleEntity(size_t id, std::string tag)
 	: m_id(id), m_tag(tag), m_alive(true)
 {
+}
+
+bool SimpleEntity::isAlive() const
+{
+	return m_alive;
+}
+
+const std::string& SimpleEntity::getTag() const
+{
+	return m_tag;
+}
+
+const size_t SimpleEntity::getId() const
+{
+	return m_id;
+}
+
+void SimpleEntity::destroy()
+{
+	m_alive = false;
 }
--- src/SimpleEntity.h
+++ src/SimpleEntity.h
@@ -1,20 +1,29 @@
+#pragma once
 #include <SFML/Graphics.hpp>
 #include <memory>
 #include "Components.h"
-#pragma once
+
 class SimpleEntity
 {
+	friend class EntityManager;
+
+	SimpleEntity(size_t, const std::string);
+	
+	const size_t m_id = 0;
+	const std::string m_tag = "default";
+	bool m_alive = true;
 	
 public:
-	const size_t m_id;
-	const std::string m_tag;
-	bool m_alive;
-
+	
 	std::shared_ptr<CTransform> cTransform;
 	std::shared_ptr<std::string>cName;
 	std::shared_ptr<sf::RectangleShape> cShape;
 	std::shared_ptr<CDisplayTag> cDisplayTag;
 
-	SimpleEntity(size_t, const std::string);
+	bool isAlive() const;
+	const std::string& getTag() const;
+	const size_t getId() const;
+	void destroy();
+	
 
 };
