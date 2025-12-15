ac5aed6e9e96ce15827087d8730b0d9e41dc05ac
Matt Stepita <mattomg24@gmail.com>
2025-12-14 16:19:42 -0800

    feat(src): Populate menu with actions
    Currently just prints console.

--- src/Scene_Menu.cpp
+++ src/Scene_Menu.cpp
@@ -1,6 +1,40 @@
 #include "Scene_Menu.h"
 
-void Scene_Menu::sDoAction(Action& action) {
+void Scene_Menu::init()
+{
+	registerAction(sf::Keyboard::Scancode::Space, "SELECT");
+	registerAction(sf::Keyboard::Scancode::Escape, "BACK");
+	registerAction(sf::Keyboard::Scancode::W, "UP");
+	registerAction(sf::Keyboard::Scancode::S, "DOWN");
+
+}
+
+void Scene_Menu::sDoAction(Action& action) 
+{
+	if(action.getType() == "PRESSED")
+	{
+		if (action.getName() == "SELECT")
+		{
+			std::cout << "Menu Action: SELECT" << std::endl;
+		}
+		else if (action.getName() == "BACK")
+		{
+			std::cout << "Menu Action: BACK" << std::endl;
+		}
+		else if (action.getName() == "UP")
+		{
+			std::cout << "Menu Action: UP" << std::endl;
+		}
+		else if (action.getName() == "DOWN")
+		{
+			std::cout << "Menu Action: DOWN" << std::endl;
+		}
+	}
+	else if(action.getType() == "RELEASED")
+	{
+
+
+	}
 }
 
 void Scene_Menu::update()
--- src/Scene_Menu.h
+++ src/Scene_Menu.h
@@ -1,9 +1,11 @@
+#pragma once
 #include "Scene.h"
 
 class Scene_Menu : public Scene {
 
+	void init();
 	void update();
 	void sDoAction(Action& action);
 	void sRender(sf::RenderWindow& window);
-
+	
 };
